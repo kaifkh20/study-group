13a677f577541ef32b2856382f1764ed3fe51f91
kaifsaif <kaifkhan.biz@gmail.com>
2023-08-05 22:02:26 +0530

    Error Fix

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -18,16 +18,25 @@ router.get('/home',auth,async(req,res)=>{
 
 router.get('/home/server',auth,async(req,res)=>{
     const serverName = req.query.serverName
+    
     const user = req.user
     // console.log(query);
     const server = await Server.findOne({serverName}).populate('channels')
-    const channels = server.channels
+    
+    if(server===null){
+        res.redirect('/home')
+        res.end()
+    }else{
+        const channels = server.channels
     // console.log(server.channels);
     res.render('channels',{
         serverName,
         channels,
         username : user.username
     })
+    }
+
+    
 })
 
 module.exports = router
